a22b02be423e64043f2ce75550cbc66bdd68e34c
Scott Serbousek <airetamstrm@gmail.com>
2019-10-16 15:57:31 -0400

    hrorm ResultInvocationHandler example

--- web/src/main/java/io/freedriver/autonomy/hrorm/mock/InterfaceBuilderParadigm.java
+++ web/src/main/java/io/freedriver/autonomy/hrorm/mock/InterfaceBuilderParadigm.java
@@ -4,8 +4,9 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.Executors;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.logging.Logger;
 
@@ -42,16 +43,45 @@ public class InterfaceBuilderParadigm<ENTITY> {
         return this;
     }
 
-
+    @SuppressWarnings("unchecked")
     public InterfaceBuilderParadigm<ENTITY> testBuild() {
-        columns.forEach(this::showOff);
+
+        ResultInvocationHandler<ENTITY> resultHandler = new ResultInvocationHandler<>(interfaceClass);
+        // Detection
+        columns.forEach(columnDefinition -> this.showOff(columnDefinition, resultHandler));
+
+        ENTITY entity = (ENTITY) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[] {interfaceClass}, resultHandler);
+
+        // Lets use the getters
+        columns.forEach(columnDefinition -> this.testGetter(columnDefinition, entity));
+
         return this;
     }
 
-    private void showOff(ColumnConfiguration<ENTITY,?> entityColumn) {
+    private void testGetter(ColumnConfiguration<ENTITY,?> columnDefinition, ENTITY entity) {
+        LOGGER.info("Fetching column value \""+columnDefinition.getColumnName()+"\" from " + columnDefinition.getInterfaceClass().getSimpleName());
+        LOGGER.info("Found " + String.valueOf(columnDefinition.getGetter().apply(entity)));
+    }
+
+    private <FIELD> void showOff(ColumnConfiguration<ENTITY, FIELD> entityColumn, ResultInvocationHandler<ENTITY> handler) {
         Method m = findMethodOfGetterFunction(interfaceClass, entityColumn.getGetter());
         LOGGER.info("Entity column " + entityColumn.getColumnName() + " refers to field/getter " + m.getName() +
                 " which returns " + m.getReturnType().toString() + " (expected "+entityColumn.getFieldClass().toString()+")");
+
+        // Now to set an example value.
+        if (entityColumn.getFieldClass().equals(String.class)) {
+            ColumnConfiguration<ENTITY, String> stringColumn = (ColumnConfiguration<ENTITY, String>) entityColumn;
+            if (stringColumn.getColumnName().equals("name")) {
+                handler.returnFromGetter(stringColumn.getGetter(), "ojplg");
+            } else if (stringColumn.getColumnName().equals("email")) {
+                handler.returnFromGetter(stringColumn.getGetter(), "ojplg@hrorm.org");
+            }
+        }
+
+        if (entityColumn.getFieldClass().equals(Long.class)) {
+            ColumnConfiguration<ENTITY, Long> longColumn = (ColumnConfiguration<ENTITY, Long>) entityColumn;
+            handler.returnFromGetter(longColumn.getGetter(), Long.MAX_VALUE);
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -80,6 +110,26 @@ public class InterfaceBuilderParadigm<ENTITY> {
         public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
             throw new MethodInvokedException(method);
         }
+    }
+
+    private static class ResultInvocationHandler<ENTITY> implements InvocationHandler {
+        private final Class<ENTITY> interfaceClass;
+        private final Map<Method, Object> values = new HashMap<>();
+
+        public ResultInvocationHandler(Class<ENTITY> interfaceClass) {
+            this.interfaceClass = interfaceClass;
+        }
+
+        @Override
+        public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+            return values.get(method);
+        }
+
+        public <FIELD> ResultInvocationHandler<ENTITY> returnFromGetter(Function<ENTITY, FIELD> getter, FIELD value) {
+            values.put(findMethodOfGetterFunction(interfaceClass, getter), value);
+            return this;
+        }
+
     }
 
 }
