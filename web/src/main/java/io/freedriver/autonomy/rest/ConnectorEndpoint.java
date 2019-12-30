0661c998360a57cb2e9419b83c47a8c6c7b679b4
Scott Serbousek <airetamstrm@gmail.com>
2019-12-29 21:43:46 -0500

    ConnectorService log output

--- web/src/main/java/io/freedriver/autonomy/rest/ConnectorEndpoint.java
+++ web/src/main/java/io/freedriver/autonomy/rest/ConnectorEndpoint.java
@@ -9,6 +9,7 @@ import io.freedriver.autonomy.service.crud.PinGroupService;
 import io.freedriver.jsonlink.jackson.schema.v1.Response;
 import org.dizitart.no2.NitriteId;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -18,8 +19,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@RequestScoped
 public class ConnectorEndpoint implements ConnectorEndpointApi {
 
     @Inject
@@ -30,7 +33,9 @@ public class ConnectorEndpoint implements ConnectorEndpointApi {
 
     @Override
     public List<BoardNameEntity> allBoardNames() {
-        return connectorService.allBoardNames();
+        return Optional.ofNullable(connectorService)
+                .map(ConnectorService::allBoardNames)
+                .orElseThrow(() -> new WebApplicationException("NULL connectorService!", 500));
     }
 
     @Override
