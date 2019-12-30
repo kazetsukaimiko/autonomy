3907ead2138962138cd8156889127c9cf76ed9bf
Scott Serbousek <airetamstrm@gmail.com>
2019-12-29 21:15:36 -0500

    ObjectMapper Context Resolver

---
+++ web/src/main/java/io/freedriver/autonomy/cdi/provider/ObjectMapperContextResolver.java
@@ -0,0 +1,24 @@
+package io.freedriver.autonomy.cdi.provider;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.dizitart.no2.mapper.NitriteIdModule;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper mapper;
+
+    public ObjectMapperContextResolver(ObjectMapper mapper) {
+        this.mapper = new ObjectMapper()
+            .registerModule(new NitriteIdModule());
+    }
+
+
+    @Override
+    public ObjectMapper getContext(Class<?> aClass) {
+        return mapper;
+    }
+}
