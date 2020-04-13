35a648d304779de2d2ac1d7c137a16d5a50fd4d5
Scott Serbousek <airetamstrm@gmail.com>
2020-04-12 21:11:30 -0400

    VEDirectMessage Index

--- web/src/main/java/io/freedriver/autonomy/vedirect/NitriteVEDirectMessage.java
+++ web/src/main/java/io/freedriver/autonomy/vedirect/NitriteVEDirectMessage.java
@@ -1,11 +1,19 @@
 package io.freedriver.autonomy.vedirect;
 
 import kaze.victron.VEDirectMessage;
+import org.dizitart.no2.IndexType;
 import org.dizitart.no2.NitriteId;
 import org.dizitart.no2.objects.Id;
+import org.dizitart.no2.objects.Index;
+import org.dizitart.no2.objects.Indices;
+import org.dizitart.no2.objects.InheritIndices;
 
 import java.util.Objects;
 
+@InheritIndices
+@Indices({
+        @Index(value="timestamp", type= IndexType.NonUnique)
+})
 public class NitriteVEDirectMessage extends VEDirectMessage {
     @Id
     private NitriteId nitriteId;
