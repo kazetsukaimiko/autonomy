4edf7435afd6b4a72a223bd9f9c077090679a35b
Scott Serbousek <airetamstrm@gmail.com>
2020-05-21 22:54:37 -0400

    Workaround for DB reads

--- jpa/src/main/java/io/freedriver/autonomy/jpa/converter/FirmwareVersionConverter.java
+++ jpa/src/main/java/io/freedriver/autonomy/jpa/converter/FirmwareVersionConverter.java
@@ -5,9 +5,11 @@ import kaze.victron.FirmwareVersion;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Converter(autoApply = true)
 public class FirmwareVersionConverter implements AttributeConverter<FirmwareVersion, String> {
+    private static final Logger LOGGER = Logger.getLogger(FirmwareVersionConverter.class.getName());
     @Override
     public String convertToDatabaseColumn(FirmwareVersion firmwareVersion) {
         return Optional.ofNullable(firmwareVersion)
@@ -17,8 +19,14 @@ public class FirmwareVersionConverter implements AttributeConverter<FirmwareVers
 
     @Override
     public FirmwareVersion convertToEntityAttribute(String s) {
-        return Optional.ofNullable(s)
-                .map(FirmwareVersion::new)
-                .orElse(null);
+        if (s != null) {
+            try {
+                return new FirmwareVersion(s);
+            } catch (IllegalArgumentException iae) {
+                // TODO LOGGER.info("")
+                return null;
+            }
+        }
+        return null;
     }
 }
