c322e656495574a6b7467725fcce5599ae1a03a7
Scott Serbousek <airetamstrm@gmail.com>
2020-06-04 20:44:32 -0400

    Embedded Event metadata, join on Event

--- web/src/main/java/io/freedriver/autonomy/vedirect/VEDirectMessageService.java
+++ web/src/main/java/io/freedriver/autonomy/vedirect/VEDirectMessageService.java
@@ -25,12 +25,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -244,6 +239,6 @@ public class VEDirectMessageService extends JPACrudService<VEDirectMessage> {
 
     @Override
     public Class<VEDirectMessage> getEntityClass() {
-        return null;
+        return VEDirectMessage.class;
     }
 }
