d02c2c52d3d648ad4985ae102b5479125effb795
Scott Serbousek <airetamstrm@gmail.com>
2020-06-07 20:43:41 -0400

    Better selection of serial resources

--- web/src/main/java/io/freedriver/autonomy/service/SBMSEventService.java
+++ web/src/main/java/io/freedriver/autonomy/service/SBMSEventService.java
@@ -3,9 +3,11 @@ package io.freedriver.autonomy.service;
 import io.freedriver.autonomy.jpa.entity.event.sbms.SBMSMessage;
 import io.freedriver.autonomy.service.crud.EventCrudService;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Default;
 
+@ApplicationScoped
 public class SBMSEventService extends EventCrudService<SBMSMessage> {
     @Override
     public Class<SBMSMessage> getEntityClass() {
