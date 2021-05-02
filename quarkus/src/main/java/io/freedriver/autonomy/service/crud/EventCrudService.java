04fb2fff358453eadf8b8b849c312ebfa7c4151c
Scott Serbousek <airetamstrm@gmail.com>
2021-05-01 23:19:33 -0400

    Config TTL

--- quarkus/src/main/java/io/freedriver/autonomy/service/crud/EventCrudService.java
+++ quarkus/src/main/java/io/freedriver/autonomy/service/crud/EventCrudService.java
@@ -1,11 +1,16 @@
 package io.freedriver.autonomy.service.crud;
 
+import io.freedriver.autonomy.Autonomy;
+import io.freedriver.autonomy.jaxrs.ObjectMapperContextResolver;
 import io.freedriver.autonomy.jpa.entity.event.Event;
 import io.freedriver.autonomy.jpa.entity.event.Event_;
+import io.freedriver.base.util.file.DirectoryProviders;
+import io.freedriver.jsonlink.config.v2.Mappings;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.Root;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -28,10 +33,32 @@ public abstract class EventCrudService<E extends Event> extends JPACrudService<E
                 .toInstant(ZoneOffset.UTC);
     }
 
+    public Mappings getMappings() throws IOException {
+        return ObjectMapperContextResolver.getMapper().readValue(
+                DirectoryProviders.CONFIG
+                        .getProvider()
+                        .subdir(Autonomy.DEPLOYMENT)
+                        .file("mappings_v2.json")
+                        .get()
+                        .toFile(),
+                Mappings.class);
+    }
+
+    public Duration getTTL() {
+        try {
+            Mappings mappings = getMappings();
+            return Duration.of(mappings.getEventTTL(), mappings.getEventTTLUnit());
+        } catch (IOException ioe) {
+            LOGGER.log(Level.WARNING, "Cannot read TTL:", ioe);
+            return Duration.ofDays(7);
+        }
+
+    }
+
     @Override
     protected E persist(E event) {
         E evt = super.persist(event);
-        int culled = applyTTL(Duration.ofDays(1));
+        int culled = applyTTL(getTTL());
         LOGGER.log(Level.INFO, culled + " removed applying TTL.");
         return evt;
     }
