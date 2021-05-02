41232a411d8a142664a75f83dfe765d2e1c817e2
Scott Serbousek <airetamstrm@gmail.com>
2021-05-01 22:02:01 -0400

    Hardcoded Event TTL

--- quarkus/src/main/java/io/freedriver/autonomy/service/crud/EventCrudService.java
+++ quarkus/src/main/java/io/freedriver/autonomy/service/crud/EventCrudService.java
@@ -1,14 +1,21 @@
 package io.freedriver.autonomy.service.crud;
 
 import io.freedriver.autonomy.jpa.entity.event.Event;
+import io.freedriver.autonomy.jpa.entity.event.Event_;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.Root;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public abstract class EventCrudService<E extends Event> extends JPACrudService<E> {
+    private final Logger LOGGER = Logger.getLogger(getClass().getName());
     public static Instant getStartOfDay() {
         LocalDateTime localDateTime = LocalDateTime.now().toLocalDate().atStartOfDay();
         return localDateTime.toInstant(ZoneOffset.UTC);
@@ -20,4 +27,25 @@ public abstract class EventCrudService<E extends Event> extends JPACrudService<E
                 .minus(duration)
                 .toInstant(ZoneOffset.UTC);
     }
+
+    @Override
+    protected E persist(E event) {
+        E evt = super.persist(event);
+        int culled = applyTTL(Duration.ofDays(1));
+        LOGGER.log(Level.INFO, culled + " removed applying TTL.");
+        return evt;
+    }
+
+    public int applyTTL(Duration duration) {
+        LOGGER.log(Level.FINEST, "Applying TTL");
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaDelete<E> delete = cb.createCriteriaDelete(getEntityClass());
+        Root<E> root = delete.from(getEntityClass());
+        long inThePast = Instant.now().minus(duration).toEpochMilli();
+        return entityManager.createQuery(
+                delete.where(cb.le(root.get(Event_.timestamp), inThePast)))
+                .executeUpdate();
+    }
+
+
 }
